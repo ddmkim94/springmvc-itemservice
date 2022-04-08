9bcb47ebc3034a0ad377aed9aa9bed4357002c05
ddmkim <ddmkim94@gmail.com>
2022-04-08 15:24:09 +0900

    주문 화면 & 메서드 추가

---
+++ src/main/java/hello/itemservice/controller/OrderController.java
@@ -0,0 +1,45 @@
+package hello.itemservice.controller;
+
+import hello.itemservice.domain.Member;
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.service.ItemService;
+import hello.itemservice.service.MemberService;
+import hello.itemservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findAll();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders"; // 주문 후 주문 목록으로 넘어감
+    }
+}
