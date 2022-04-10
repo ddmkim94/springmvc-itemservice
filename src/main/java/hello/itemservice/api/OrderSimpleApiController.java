4a014dc183086a330512f2407cae18abe1eece5e
ddmkim <ddmkim94@gmail.com>
2022-04-10 15:32:06 +0900

    간단 주문 조회 V2 - 엔티티를 DTO로 변환

--- src/main/java/hello/itemservice/api/OrderSimpleApiController.java
+++ src/main/java/hello/itemservice/api/OrderSimpleApiController.java
@@ -1,12 +1,18 @@
 package hello.itemservice.api;
 
+import hello.itemservice.domain.Address;
 import hello.itemservice.domain.Order;
 import hello.itemservice.domain.OrderSearch;
+import hello.itemservice.domain.OrderStatus;
 import hello.itemservice.repository.OrderRepository;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -14,6 +20,17 @@ import java.util.List;
 public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v2/simple-orders")
+    public Yeonseo<List<SimpleOrderDTO>> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDTO> result = new ArrayList<>();
+
+        for (Order order : orders) {
+            result.add(new SimpleOrderDTO(order));
+        }
+        return new Yeonseo<>(result.size(), result);
+    }
 
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
@@ -23,5 +40,31 @@ public class OrderSimpleApiController {
             order.getDelivery().getAddress(); // LAZY 강제 초기화
         }
         return orders;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Yeonseo<T> {
+        private int count;
+        private T data;
+    }
+
+
+    // 주문 조회용 DTO
+    @Data
+    static class SimpleOrderDTO {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDTO(Order order) {
+            this.orderId = order.getId();
+            this.name = order.getMember().getName(); // LAZY 초기화
+            this.orderDate = order.getOrderDate();
+            this.orderStatus = order.getOrderStatus();
+            this.address = order.getDelivery().getAddress(); // LAZY 초기화
+        }
     }
 }
